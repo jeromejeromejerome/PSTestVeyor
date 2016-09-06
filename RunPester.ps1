e6f7968f452cdc12bff6f56ed958433d49f9312a
Fichelle <JP004546@firm.jonesday.net>
2016-09-06 16:28:00 -0400

    yeah

--- RunPester.ps1
+++ RunPester.ps1
@@ -3,5 +3,5 @@
 #
 
 #Import-Module "$PSScriptRoot\Pester\Pester.psm1"
-Invoke-Pester -CodeCoverage *.ps1 -Path "$PSScriptRoot\PSExample" -OutputFormat NUnitXml -OutputFile "$PSScriptRoot\TestResult.xml"
+Invoke-Pester -CodeCoverage *.ps1 -Path "$PSScriptRoot\PSExample" -OutputFormat NUnitXml -OutputFile "$PSScriptRoot\TestResult.xml" -PassThru
 
